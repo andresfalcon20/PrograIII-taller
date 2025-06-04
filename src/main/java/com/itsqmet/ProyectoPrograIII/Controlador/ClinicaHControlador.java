e18cdbfed05b27ac69d8424a10d17bf5f4fc6b99
andresfalcon.20 <andresfalcon.18@hotmail.com>
2025-06-04 10:08:27 -0500

    inicio

--- src/main/java/com/itsqmet/ProyectoPrograIII/Controlador/ClinicaHControlador.java
+++ src/main/java/com/itsqmet/ProyectoPrograIII/Controlador/ClinicaHControlador.java
@@ -1,4 +1,14 @@
 package com.itsqmet.ProyectoPrograIII.Controlador;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ClinicaHControlador {
+    @GetMapping("/index")
+    public String mostrarHome(){
+        return "index";
+    }
+
 }
